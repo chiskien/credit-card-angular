82809bdacc3d2027205c6e21c39ebdba67e0f155
ChisKien <chiskien03214@gmail.com>
2021-09-21 09:26:25 +0700

    validators for expiration and other input

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -9,8 +9,12 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
 export class CardFormComponent implements OnInit {
   cardForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    card: new FormControl(''),
-    expiration: new FormControl(''),
+    card: new FormControl('',
+      [Validators.required, Validators.minLength[16],Validators.maxLength[16]
+      ]),
+    expiration: new FormControl('',[
+      Validators.required, Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+    ]),
     securityCode: new FormControl(''),
   });
 
